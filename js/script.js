f964ba7c682ae9982888dba960653e8863bb100e
carlanaso <158155301+carlanaso@users.noreply.github.com>
2024-05-28 06:05:30 +0000

    change again2

--- js/script.js
+++ js/script.js
@@ -13,7 +13,36 @@ var typed = new Typed (".writing",{
     BackSpeed: 60
 
 })
+const faders = document.querySelectorAll(".fade-in");
+const sliders = document.querySelectorAll(".slide-in");
 
+const appearOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px -250px 0px"
+  };
+  
+  const appearOnScroll = new IntersectionObserver(function(
+    entries,
+    appearOnScroll
+  ) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        return;
+      } else {
+        entry.target.classList.add("appear");
+        appearOnScroll.unobserve(entry.target);
+      }
+    });
+  },
+  appearOptions);
+  
+  faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+  });
+  
+  sliders.forEach(slider => {
+    appearOnScroll.observe(slider);
+  });
 
 document.querySelector('.scroll-down').addEventListener('click', function() {
     // Get the .home section element
